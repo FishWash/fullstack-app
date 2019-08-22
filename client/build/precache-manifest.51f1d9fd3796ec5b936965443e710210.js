b235c518e510d6e6f11892d9fe51a8999b0482fa
Jonathan Fox <FoxJonathanP@gmail.com>
2019-08-22 14:31:40 -0700

    Made client build

---
+++ client/build/precache-manifest.51f1d9fd3796ec5b936965443e710210.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9edeebe172b39bfc4f1e847762c75c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec56bd032ae673b24ef9",
+    "url": "/static/js/2.4577bbfa.chunk.js"
+  },
+  {
+    "revision": "ca24bd987bb713e706be",
+    "url": "/static/js/main.b0c793bc.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  }
+]);
